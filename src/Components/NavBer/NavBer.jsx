b3a67a3a616cf52299680cc7c29bee78f3b70543
shahidul-islam <shahidulislam47326@gmail.com>
2025-12-16 22:44:47 +0600

    navber search ber update

--- src/Components/NavBer/NavBer.jsx
+++ src/Components/NavBer/NavBer.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { IoMenu } from "react-icons/io5";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoSearch } from "react-icons/io5";
@@ -8,39 +8,37 @@ import { Link, NavLink } from "react-router-dom";
 const NavBer = () => {
   const [open, setOpen] = useState(false);
   const [showSearch, setShowSearch] = useState(false);
-  const [htmlOpen, setHtmlOpen] = useState(false);
   const [jsOpen, setJsOpen] = useState(false);
-  const [moreOpen, setMoreOpen] = useState(false);
+  const inputRef = useRef(null);
+
+  const handleSearch = (e) => {
+    if (e.key === "Enter") {
+      console.log("Search value:", e.target.value);
+      setShowSearch(false);
+      e.target.value = "";
+    }
+  };
 
   const NavBer = (
     <>
-      <li className="text-nu20">
+      <li className="text-nu10">
         <NavLink to={"/"}>HOME</NavLink>
       </li>
+
       <li className="relative group cursor-pointer">
-        <div className="flex items-center gap-1 text-nu20">
-          HTML & CSS <IoIosArrowDown className="" />
-        </div>
-        <ul className="absolute top-full left-0 hidden group-hover:block bg-nu10 rounded shadow mt-0.5 w-48">
-          <li className="px-4 py-2 text-nu20 bg-nu10">Web Design</li>
-          <li className="px-4 py-2 text-nu20 bg-nu10">Login Forms</li>
-          <li className="px-4 py-2 text-nu20 bg-nu10">Card Design</li>
-        </ul>
-      </li>
-      <li className="relative group cursor-pointer">
-        <div className="flex items-center gap-1 text-nu20">
+        <div className="flex items-center gap-1 text-nu10">
           JAVASCRIPT <IoIosArrowDown />
         </div>
-        <ul className="absolute top-full left-0 hidden group-hover:block bg-[#3E8DA8] rounded shadow mt-0.5 w-52">
+        <ul className="absolute top-full left-0 hidden group-hover:block rounded shadow mt-0.5 w-52">
           <li className="px-4 py-2 text-nu20 bg-nu10">Dynamic Clock</li>
           <li className="px-4 py-2 text-nu20 bg-nu10">Form Validation</li>
           <li className="px-4 py-2 text-nu20 bg-nu10">Card Slider</li>
         </ul>
       </li>
-      <li className="text-nu20">
+      <li className="text-nu10">
         <NavLink to={"/about"}>ABOUT US</NavLink>
       </li>
-      <li className="text-nu20">CONTACT US</li>
+      <li className="text-nu10">CONTACT US</li>
     </>
   );
 
@@ -51,15 +49,17 @@ const NavBer = () => {
           <button className="lg:hidden text-nu20" onClick={() => setOpen(true)}>
             <IoMenu className="text-[24px]" />
           </button>
-          <div className="text-nu20 text-2xl font-semibold">Logo</div>
+          <div className="text-nu10 text-2xl font-semibold">
+            <h3>GroupStudy</h3>
+          </div>
           <ul className="hidden lg:flex items-center gap-6 text-nu10  font-medium">
             {NavBer}
           </ul>
           <div className="flex justify-between items-center gap-4">
-            <div className="relative">
+            {/* <div className="relative">
               <button
                 onClick={() => setShowSearch(!showSearch)}
-                className="text-nu20"
+                className="text-nu10"
               >
                 <IoSearch className="text-[24px]" />
               </button>
@@ -69,6 +69,30 @@ const NavBer = () => {
                     type="text"
                     placeholder="Search..."
                     className="w-[400px] px-3 py-2 rounded outline-none"
+                  />
+                </div>
+              )}
+            </div> */}
+            <div className="relative">
+              <button
+                onClick={() => {
+                  setShowSearch(true);
+                  setTimeout(() => inputRef.current?.focus(), 100);
+                }}
+                className="text-nu10"
+              >
+                <IoSearch className="text-[24px]" />
+              </button>
+
+              {showSearch && (
+                <div className="absolute right-0 mt-4 bg-nu10 p-3 rounded shadow">
+                  <input
+                    ref={inputRef}
+                    type="text"
+                    placeholder="Search..."
+                    className="w-[400px] px-3 py-2 rounded outline-none"
+                    onKeyDown={handleSearch}
+                    onBlur={() => setShowSearch(false)}
                   />
                 </div>
               )}
