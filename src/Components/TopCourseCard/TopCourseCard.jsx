0abe74054c46aea7da6b5fe24f1547c39f5d0525
shahidul-islam <shahidulislam47326@gmail.com>
2025-12-22 21:24:15 +0600

    new update

--- src/Components/TopCourseCard/TopCourseCard.jsx
+++ src/Components/TopCourseCard/TopCourseCard.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { FaStar } from "react-icons/fa";
 
 const TopCourseCard = ({ course }) => {
-  const { image } = course;
+  const { image, coursesName, author, authorImg, rating, price, time, level } =
+    course;
   return (
     <>
       <div className="px-4 py-4 bg-nu60">
         <img className="w-full h-full" src={image} alt="" />
-        <div className=""></div>
+        <div className="">
+          <div className="">
+            <span>{rating}</span>
+            <div className="">
+              <FaStar />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
