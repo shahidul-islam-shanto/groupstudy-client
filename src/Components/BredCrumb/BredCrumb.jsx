feb663d8149c0a5f99d529e0b8855cc9ca49d2e9
shahidul-islam <shahidulislam47326@gmail.com>
2025-12-23 22:18:42 +0600

    course update and bred crumb page add

---
+++ src/Components/BredCrumb/BredCrumb.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./BredCrumb.css";
+import checkout from "../../assets/images/bred-crumb.png";
+import { Link } from "react-router-dom";
+import { IoIosArrowForward } from "react-icons/io";
+
+const BredCrumb = ({ bredCrumb }) => {
+  return (
+    <>
+      <div className="">
+        <div className="relative ">
+          <img src={checkout} className="w-full " />
+          <div className="absolute inset-0 bg-[linear-gradient(91deg,rgba(6,7,7,0.600)_0.97%,rgba(2,2,2,0.600)_100%)]"></div>
+        </div>
+        <div className="absolute top-65 left-60 text-nu10 ">
+          <h1 className="text-nu10 font-semibold mb-4">{bredCrumb}</h1>
+          <div className="flex items-center gap-2">
+            <Link
+              to={"/"}
+              className="hover:text-primary1 font-medium duration-300"
+            >
+              Home
+            </Link>
+            <IoIosArrowForward className="text-nu10 font-bold text-[20px]" />
+            <span>Course Details</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BredCrumb;
--- src/Pages/TopCourseDetails/TopCourseDetails.jsx
+++ src/Pages/TopCourseDetails/TopCourseDetails.jsx
@@ -1,22 +1,40 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import BredCrumb from "../../Components/BredCrumb/BredCrumb";
 
 const TopCourseDetails = () => {
   const courses = useLoaderData();
-  console.log(courses);
+  // console.log(courses);
 
-  const { image } = courses;
+  const { image, rating, price, time, level, author, authorImg } = courses;
 
   return (
-    <div>
+    <>
       <div className="">
+        <BredCrumb bredCrumb={"Course Details"} />
+      </div>
+      <div className="xl:py-[120px] lg:pt-[100px] md:pt-[90px] sm:pt-20 pt-[70px]">
         <div className="container-2">
           <div className="">
-            <img src={image} alt="" />
+            <img
+              className="w-full h-[600px] rounded-xl object-cover"
+              src={image}
+              alt=""
+            />
           </div>
+          {/* <div className="grid grid-cols-12 gap-6">
+            <div className="col-span-8">
+              <div className="">
+                <div className="">
+                  <img src={authorImg} alt="" />
+                  <span>{author}</span>
+                </div>
+              </div>
+            </div>
+          </div> */}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
